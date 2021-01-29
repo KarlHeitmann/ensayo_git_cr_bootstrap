282b40a2abe8c2788264b6f9d02e6c7c73208f55
Elian Rojas <impresosgosen@gmail.com>
2021-01-28 20:52:00 -0600

    Aporte Elian Rojas

---
+++ elian.rb
@@ -0,0 +1,35 @@
+
+
+seleccion = "Elian Roberto Rojas Rojas"
+
+discos = {
+  "Alonso Viales Arrieta" => "This Was (1968)",
+  "Cynthia Johanna Salazar Rodrguez" => "Stand Up (1969)",
+  "Elian Roberto Rojas Rojas" => "Benefit (1970)",
+  "Erick Ramos Mora" => "Aqualung (1971)",
+  "Erick Rojas Arias" => "Thick as a Brick (1972)",
+  "Fabián Conejo Segura" => "A Passion Play (1973)",
+  "Flor de Azalea Marcia Acua" => "War Child (1974)",
+  "Gabriela Sols Chavarra" => "Minstrel in the Gallery (1975)",
+  "Henry Artavia Chavarria" => "Too Old to Rock 'n' Roll: Too Young to Die! (1976)",
+  "Jacqueline de los Ángeles Picado Azofeifa" => "Songs from the Wood (1977)",
+  "Jordan Sandoval Gonzalez" => "Heavy Horses (1978)",
+  "Joseph Murillo Rodrguez" => "Stormwatch (1979)",
+  "Lidia Arce Picado" => "A (1980)",
+  "Mara Roxana Chinchilla Vsquez" => "The Broadsword and the Beast (1982)",
+  "Marcela de los Ángeles Brenes Daniels" => "Under Wraps (1984)",
+  "Maria Alejandra Serrano Ramrez" => "Crest of a Knave (1987)",
+  "Mauricio Badilla Blen" => "Rock Island (1989)",
+  "Oscar Vega Venegas" => "Catfish Rising (1991)",
+  "Pablo Mora Salazar" => "Roots to Branches (1995)",
+  "Randall Chaves Camacho" => "J-Tull Dot Com (1999)",
+  "Rogelio Chanto segura" => "The Jethro Tull Christmas Album (2003)",
+  "Shirley María Alvarado Soto" => "Walk into Light   Chrysalis/EMI Records   202   78   ",
+  "Steyler Ayala Palacios" => "Divinities: Twelve Dances with God   Angel/EMI Records       ",
+  "Tatiana Mara Lara Martnez" => "The Secret Language of Birds   Fuel 2000/Varèse Sarabande/Universal Records   26     ",
+  "Yirlania Torres Salas" => "Rupi's Dance   RandM Records       40",
+  "Karl Heitmann" => "Thick as a Brick 2",
+  "Sebastián Jiménez" => "Homo Erraticus"
+}
+
+puts discos[seleccion]
